02f701ba3f1bb41de659e2a939c5b431a04d4145
Port31337 <potr31337@gmail.com>
2018-09-20 23:06:55 +0300

    Task 4.4

--- 04_data_structures/task_4_4.py
+++ 04_data_structures/task_4_4.py
@@ -14,3 +14,12 @@
 
 command1 = 'switchport trunk allowed vlan 1,3,10,20,30,100'
 command2 = 'switchport trunk allowed vlan 1,3,100,200,300'
+vlans1 = command1.strip().split()
+vlans1 = vlans1[-1].split(',')
+vlans2 = command2.strip().split()
+vlans2 = vlans2[-1].split(',')
+r = set(vlans2).intersection(set(vlans1))
+r = list(r)
+r = [int(i) for i in r]
+r.sort()
+print(r)
